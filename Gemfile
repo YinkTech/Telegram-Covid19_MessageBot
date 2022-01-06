5a2439c566c18dcfbee283dd2b21390f988f14e4
YinkTech <layinka4dat@gmail.com>
2022-01-06 16:08:00 +0100

    linter file upload

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'telegram-bot'
 gem 'rspec'
+gem 'telegram-bot'
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/bot'
 
-describe Data do 
+describe Data do
   describe '#select_current' do
     it 'it return true if a value is passed' do
       expect(@value = value).to eql true
