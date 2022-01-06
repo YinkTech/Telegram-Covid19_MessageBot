3f6b08ecaf8dc8a5d53a9ef5c68b9b49ff268241
YinkTech <layinka4dat@gmail.com>
2022-01-06 15:55:15 +0100

    test the methods

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,4 +1,9 @@
-#require_relative '../lib/data.rb'
-describe Data do
+require_relative '../lib/bot'
 
+describe Data do 
+  describe '#select_current' do
+    it 'it return true if a value is passed' do
+      expect(@value = value).to eql true
+    end
+  end
 end
